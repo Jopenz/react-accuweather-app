5bb6a6a2544b4977c26af8318cb59f475895c68c
Adrin Martinez <adrian.martinez@waapiti.eu>
2020-01-16 11:58:38 +0100

    add search box

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Paper, InputBase } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
 
 class Home extends Component {
     constructor(props) {
@@ -8,7 +11,19 @@ class Home extends Component {
     render() {
         return (
             <div className='flex flex-col w-full h-full'>
-                
+                <div className='flex justify-center content-center items-center'>
+                    <Paper style={{ width : 320 }} className='flex m-20 pl-6 justify-between' >
+                        <InputBase
+                            fullWidth
+                            placeholder="Search City"
+                            inputProps={{ 'aria-label': 'search city' }}
+                        />
+                         <IconButton type="submit" className={''} aria-label="search">
+                            <SearchIcon />
+                        </IconButton>
+                    </Paper>
+                </div>
+                <div className='flex flex-1'></div>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -2,20 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import App from './App';
 import rootReducer from './store/reducers'
 import * as serviceWorker from './serviceWorker';
 import { ThemeProvider } from '@material-ui/core';
 import theme from './theme/theme';
+import './tailwind.css';
 
 const store = createStore(rootReducer)
 
 ReactDOM.render(
     <Provider store={store}>
         <ThemeProvider theme={theme}>
+            <CssBaseline />
             <App />
         </ThemeProvider>
-    </Provider>, document.getElementById('root'));
+    </Provider>, document.querySelector('#app'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
