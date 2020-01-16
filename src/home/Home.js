ba466a99647d456d1d5a931ff6d5ba28c7152bf0
Adrin Martinez <adrian.martinez@waapiti.eu>
2020-01-16 11:06:51 +0100

    first commit

---
+++ src/home/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            <div className='flex flex-col w-full h-full'>
+                
+            </div>
+        );
+    }
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
+import rootReducer from './store/reducers'
 import * as serviceWorker from './serviceWorker';
+import { ThemeProvider } from '@material-ui/core';
+import theme from './theme/theme';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
